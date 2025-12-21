60e4a77678e61b98574e9b4b96740d07656ca93e
sohil raval <sohil.mscit@gmail.com>
2025-12-21 19:19:47 +0530

    teest

--- MyConsoleApp/Animal.cs
+++ MyConsoleApp/Animal.cs
@@ -5,5 +5,6 @@ public class Animal
     public virtual void Speak()
     {
         Console.WriteLine("The animal makes a sound.");
+         Console.WriteLine("The animal makes a sound_2.");
     }
 }
--- MyConsoleApp/obj/Debug/net8.0/MyConsoleApp.AssemblyInfo.cs
+++ MyConsoleApp/obj/Debug/net8.0/MyConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08d560c64b5f4e1a036a29c57b2db7b40b8239b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe8b59937ed5436896f13246db3d78aa828059b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
